3082abc0d761fff3466634e256ce53a3ca2d2c18
YuliaBondar <uliabondar359@gmail.com>
2023-12-13 00:00:22 +0300

    tesst

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,8 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  const sortedHeights = [];
+  const positiveHeights = arr.filter(height => height !== -1);
+
+  positiveHeights.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      sortedHeights.push(-1);
+    } else {
+      sortedHeights.push(positiveHeights.shift());
+    }
+  }
+
+  return sortedHeights;
+
   // remove line with error and write your code here
 }
 
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,8 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function getSumOfDigits(n) {
+  let sum = n;
+
+  while (sum >= 10) {
+    let digitsSum = 0;
+
+    const sumString = String(sum);
+    for (let i = 0; i < sumString.length; i++) {
+      digitsSum += parseInt(sumString[i]);
+    }
+
+    sum = digitsSum;
+  }
+
+  return sum;
   // remove line with error and write your code here
 }
 
